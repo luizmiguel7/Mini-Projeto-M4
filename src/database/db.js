a24a2d9e4a853a973085540a67a7c6da330bf01b
Luiz Miguel <luizmiguel.zx7@gmail.com>
2025-06-26 15:44:47 -0300

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -6,7 +6,7 @@ const database = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
+      rejectUnauthorized: false,
     }
   }
 });
